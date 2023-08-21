a31c77079ec5782b70916340b1f51ddf9cad47c6
miranda utami <mirandautami01@student.ciputra.ac.id>
2023-08-21 12:03:21 +0700

    Initial Commit

---
+++ SholatTimeApp/SholatTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SholatTimeAppApp.swift
+//  SholatTimeApp
+//
+//  Created by miranda on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SholatTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
